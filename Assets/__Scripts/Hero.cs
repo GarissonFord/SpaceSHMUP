beed5faa7dfaea54695bc55cbc067dfd6e1a3e3c
Gary DiCiano <garydiciano4@gmail.com>
2017-06-26 20:26:33 -0400

    Adjusted Hero Inspector Parameters
    Creates a game feel better suited for me

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -1,32 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Hero : MonoBehaviour {
-
+public class Hero : MonoBehaviour 
+{
 	static public Hero		S;
 
 	public float	speed = 30;
 	public float	rollMult = -45;
-	public float  	pitchMult=30;
+	public float  	pitchMult= 30;
 
-	public float	shieldLevel=1;
+	public float	shieldLevel = 1;
 
 	public bool	_____________________;
 	public Bounds bounds;
 
-	void Awake(){
+	void Awake()
+	{
 		S = this;
 		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
 	}
 
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		float xAxis = Input.GetAxis("Horizontal");
 		float yAxis = Input.GetAxis("Vertical");
 
@@ -38,13 +41,14 @@ public class Hero : MonoBehaviour {
 		bounds.center = transform.position;
 		
 		// constrain to screen
-		Vector3 off = Utils.ScreenBoundsCheck(bounds,BoundsTest.onScreen);
-		if (off != Vector3.zero) {  // we need to move ship back on screen
+		Vector3 off = Utils.ScreenBoundsCheck(bounds, BoundsTest.onScreen);
+		if (off != Vector3.zero) 
+		{  // we need to move ship back on screen
 			pos -= off;
 			transform.position = pos;
 		}
 		
 		// rotate the ship to make it feel more dynamic
-		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
+		transform.rotation = Quaternion.Euler(yAxis*pitchMult, xAxis * rollMult, 0);
 	}
 }
